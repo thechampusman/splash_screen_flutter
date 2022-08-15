bd1f878211c3d63f75f63d17c9b7aa208e0c0396
champ0011 <usmangour100@gmail.com>
2022-08-16 03:22:56 +0530

    16/08

---
+++ android/app/src/main/java/com/thechamp/splash/splash_screen_thechamp/MainActivity.java
@@ -0,0 +1,6 @@
+package com.thechamp.splash.splash_screen_thechamp;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/splashscreen.dart
@@ -0,0 +1,48 @@
+// ignore_for_file: camel_case_types
+
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:splash_screen_thechamp/home.dart';
+
+class Splash_Screen extends StatefulWidget {
+  const Splash_Screen({Key? key}) : super(key: key);
+
+  @override
+  State<Splash_Screen> createState() => _Splash_ScreenState();
+}
+
+class _Splash_ScreenState extends State<Splash_Screen> {
+  @override
+  void initState() {
+    super.initState();
+    // Timer(
+    //     const Duration(seconds: 3),
+    //     () => Navigator.pushReplacement(
+    //         context, MaterialPageRoute(builder: (context) => const Home())));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      backgroundColor: Color(0xFF169FD8),
+      body: Column(
+        children: [
+          SizedBox(
+            height: size.height / 6,
+          ),
+          Container(
+            height: size.height / 1.5,
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+                image: DecorationImage(
+              image: AssetImage('assets/images/tutorial.png'),
+              fit: BoxFit.fill,
+            )),
+          ),
+        ],
+      ),
+    );
+  }
+}
