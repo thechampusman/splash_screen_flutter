13b51f871db5cfacfdb3a964b0f89a2578de2065
champ0011 <usmangour100@gmail.com>
2022-08-27 01:38:09 +0530

    27-08

--- lib/home.dart
+++ lib/home.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
@@ -8,40 +9,97 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFF2F2F4C),
-      body: Column(
-        children: [
-          SizedBox(
-            height: 80,
-          ),
-          Row(
-            children: [
-              SizedBox(
-                width: 30,
-              ),
-              Container(
-                width: 60.0,
-                height: 60.0,
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage('assets/images/tc.jpg'),
-                    fit: BoxFit.fill,
+      backgroundColor: Color.fromARGB(235, 255, 255, 255),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 45,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15),
+              child: SvgPicture.asset(
+                'assets/images/clouds.svg',
+                // height: 70.0,
+                // width: 50.0,
+                // allowDrawingOutsideViewBox: true,
+              ), //
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 45),
+              child: Row(
+                children: [
+                  Text(
+                    "Hey, Champion",
+                    style: TextStyle(
+                        fontFamily: "AnekTelugu ",
+                        fontWeight: FontWeight.bold,
+                        fontSize: 25),
                   ),
-                  borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                  // color: Colors.redAccent,
-                ),
-                // child: Image.asset('assets/images/tc.jpg'),
+                ],
               ),
-              SizedBox(
-                width: 10,
+            ),
+            SizedBox(
+              height: 180,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 50),
+              child: Stack(
+                children: [
+                  Container(
+                    height: 300,
+                    width: 190,
+                    color: Color(0xFFE8EBE8),
+                    //E8EBE8
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 50),
+                    child: Container(
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(50)),
+                      height: 240,
+                      width: 190,
+                      child: Padding(
+                        padding: const EdgeInsets.only(top: 120),
+                        child: Column(
+                          children: [
+                            Text(
+                              "Youtube",
+                              style: TextStyle(
+                                  fontFamily: "AnekTelugu ",
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 28),
+                            ),
+                            Spacer(),
+                            InkWell(
+                              child: Padding(
+                                padding: const EdgeInsets.only(bottom: 35),
+                                child: Image.asset(
+                                  "assets/images/subscribe.png",
+                                  height: 38,
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                      left: 20,
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                        child: Image.asset(
+                          "assets/images/youtube.png",
+                          height: 150,
+                        ),
+                      ))
+                ],
               ),
-              Text(
-                "Hey, Champion",
-                style: TextStyle(color: Color(0xFFB4B6CA), fontSize: 20),
-              ),
-            ],
-          )
-        ],
+            ),
+          ],
+        ),
       ),
     );
   }
---
+++ lib/iconholder.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class iconholder extends StatefulWidget {
+  const iconholder({Key? key}) : super(key: key);
+
+  @override
+  State<iconholder> createState() => _iconholderState();
+}
+
+class _iconholderState extends State<iconholder> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            height: 300,
+            width: 190,
+            color: Color(0xFFE8EBE8),
+            //E8EBE8
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 50),
+            child: Container(
+              decoration: BoxDecoration(
+                  color: Colors.white, borderRadius: BorderRadius.circular(50)),
+              height: 250,
+              width: 190,
+            ),
+          ),
+          Positioned(
+              left: 20,
+              child: ClipRRect(
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+                child: Image.asset(
+                  "assets/images/youtube.png",
+                  height: 150,
+                ),
+              ))
+        ],
+      ),
+    );
+  }
+}
